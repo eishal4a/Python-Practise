2d1f3d8fa860419a5b6342dd6f1b33ccbaee0639
Eishal <eishal.cecos@gmail.com>
2025-04-15 14:39:02 +0500

    p
    p

--- 1.py
+++ 1.py
@@ -1,2 +1,11 @@
+import demodule
+from demoduleimport add,subtract,multiply,divide
+
 def add(a,b);
-a+b
+return a+b
+def subtract(a,b)
+retur a-b
+def Multiply(a,b)
+retur a*b
+def Divide(a,b)
+retur a/b
