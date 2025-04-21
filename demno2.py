906a5b9dd53e93616f7f1fea9ebd4f823377066a
Eishal <eishal.cecos@gmail.com>
2025-04-21 15:34:02 +0500

    p
    p

---
+++ demno2.py
@@ -0,0 +1,11 @@
+
+book_info = {
+    "title": "The Alchemist",
+    "author": "Paulo Coelho",
+    "year": 1988
+}
+
+unique_colors = {"red", "blue", "green", "blue", "yellow", "red"}
+
+print("Book Info Dictionary:", book_info)
+print("Unique Colors Set:", unique_colors)
---
+++ demo3.py
@@ -0,0 +1,12 @@
+
+
+temperature = 42 
+
+if temperature >= 40:
+    print("It's a hot day!")
+elif temperature >= 30:
+    print("It's a warm day.")
+elif temperature >= 20:
+    print("It's a pleasant day.")
+else:
+    print("It's a cold day.")
