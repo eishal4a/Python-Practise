c7bc6deb6b9c54663d4efc34758216e5a3c7ed26
Eishal <eishal.cecos@gmail.com>
2025-04-21 15:31:46 +0500

    p
    p

---
+++ demo1.py
@@ -0,0 +1,12 @@
+
+name = "Sara"
+age = 19
+
+favorite_subjects = ["Math", "Biology", "English"]
+
+months = ("January", "February", "March", "April", "May")
+
+print("Name:", name)
+print("Age:", age)
+print("Favorite Subjects:", favorite_subjects)
+print("Months:", months)
